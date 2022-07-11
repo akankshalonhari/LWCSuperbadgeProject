2635bda2aef8266fe482f3a1f07c6262afbd38be
Akanksha Lonhari <alonhari@alonhar-wsmc4wy.internal.salesforce.com>
2022-07-10 18:12:32 -0700

    LWC Superbadge Project Challenge 11-14

---
+++ lwcsuperbadge_project/LWCPackage/main/default/lwc/boatDetailTabs/boatDetailTabs.js
@@ -0,0 +1,77 @@
+import { LightningElement, wire, api } from 'lwc';
+import labelDetails from '@salesforce/label/c.Details';
+import labelReviews from '@salesforce/label/c.Reviews';
+import labelAddReview from '@salesforce/label/c.Add_Review';
+import labelFullDetails from '@salesforce/label/c.Full_Details';
+import labelPleaseSelectABoat from '@salesforce/label/c.Please_select_a_boat'
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+import { MessageContext, subscribe, APPLICATION_SCOPE } from 'lightning/messageService';
+import { NavigationMixin } from 'lightning/navigation';
+import BOAT_ID_FIELD from '@salesforce/schema/Boat__c.Id';
+import BOAT_NAME_FIELD from '@salesforce/schema/Boat__c.Name';
+const BOAT_FIELDS = [BOAT_ID_FIELD, BOAT_NAME_FIELD];
+
+export default class BoatDetailTabs extends NavigationMixin(LightningElement) {
+    @api boatId;
+    @api wiredRecord;
+
+    label = {
+        labelDetails,
+        labelReviews,
+        labelAddReview,
+        labelFullDetails,
+        labelPleaseSelectABoat,
+    };
+
+    boatDetailsList = [
+        'Boat__c.Type',
+        'Boat__c.Length',
+        'Boat__c.Price',
+        'Boat__c.Description',
+    ];
+
+    @wire(MessageContext)
+    messageContext;
+
+    @wire(getRecord, { recordId: '$boatId', fields: BOAT_FIELDS })
+    wiredRecord;
+    get boatName() {
+      return getFieldValue(this.wiredRecord.data, BOAT_NAME_FIELD);
+    }
+
+    get detailsTabIconName() {
+        return this.wiredRecord && this.wiredRecord.data ? 'utility:anchor' : null;
+    }
+
+    // Subscribe to the message channel to retrieve the recordId and explicitly assign it to boatId.
+    //subscription = subscribe(this.messageContext, BOATMC, (message) => {this.boatId = message.recordId}, {scope: APPLICATION_SCOPE});
+    subscription = null;
+    
+    subscribeMC() {
+        if(this.subscription) { return; }
+        //  Subscribe to the message channel to retrieve the recordId and explicitly assign it to boatId.
+        this.subscription = subscribe(this.messageContext, BOATMC, (message) => {this.boatId = message.recordId}, {scope: APPLICATION_SCOPE});
+    }
+
+    // Calls subscribeMC()
+    connectedCallback() { 
+        this.subscribeMC();
+    }
+
+    navigateToRecordViewPage() {
+        this[NavigationMixin.Navigate]({
+            type:'standard__recordPage',
+            attributes:{
+                recordId: this.boatId,
+                actionName:'view'
+            },
+        });
+    }
+ 
+    // Navigates back to the review list, and refreshes reviews component
+    handleReviewCreated() { 
+        this.template.querySelector('lightning-tabset').activeTabValue = 'reviews';
+        this.template.querySelector('c-boat-reviews').refresh();
+    }
+}
